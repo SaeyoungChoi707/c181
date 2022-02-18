c8859e0e8c63e452a21c75a8e902722854de4871
SaeyoungChoi707 <63861341+SaeyoungChoi707@users.noreply.github.com>
2022-02-18 20:15:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Main from './screens/main'
+export default function App() {
+  return (
+    <Main/>
+  )
+}
